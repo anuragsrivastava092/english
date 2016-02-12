76af19220894e3febb1869791e3febf21ffbc845
anurag <anurag.srivastava092@gmail.com>
2016-02-13 02:24:52 +0530

    changed admin to accomodate model changes

--- english/admin.py
+++ english/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import User,article,question,choice,comment,performance,word,word_meaning
+from .models import User,article,question,comment,performance,word,word_meaning
 
 admin.site.register(User)
 admin.site.register(article)
 
 admin.site.register(question)
-admin.site.register(choice)
+
 admin.site.register(comment)
 admin.site.register(performance)
 admin.site.register(word)
